a3fb9637cad3f5f6a83da61425da7b3e013b079f
Adam M Duff <fikosan@gmail.com>
2018-05-07 10:36:54 -0400

    refactor: Files edited for spacing/formatting

--- js/app.js
+++ js/app.js
@@ -2,23 +2,23 @@
  * Create a list that holds all of your cards
  */
 
- const cardsList = [
-     'swg-xwing', 
-     'swg-xwing',
-     'swg-atat',
-     'swg-atat',
-     'swg-tie', 
-     'swg-tie',
-     'swg-reball', 
-     'swg-reball',
-     'swg-r2d2-3', 
-     'swg-r2d2-3', 
-     'swg-darthvader', 
-     'swg-darthvader',
-     'swg-stormtrooper',
-     'swg-stormtrooper',
-     'swg-deathstar', 
-     'swg-deathstar'
+const cardsList = [
+    'swg-xwing',
+    'swg-xwing',
+    'swg-atat',
+    'swg-atat',
+    'swg-tie',
+    'swg-tie',
+    'swg-reball',
+    'swg-reball',
+    'swg-r2d2-3',
+    'swg-r2d2-3',
+    'swg-darthvader',
+    'swg-darthvader',
+    'swg-stormtrooper',
+    'swg-stormtrooper',
+    'swg-deathstar',
+    'swg-deathstar'
 ];
 
 // New variable to store cards clicked/matched
@@ -39,8 +39,12 @@ let score = 3;
 // Element selector variables
 const deck = document.querySelector('.deck');
 const card = deck.querySelector('.card');
-const cardIcon = deck.querySelectorAll('.swg'); 
+const cardIcon = deck.querySelectorAll('.swg');
 const restart = document.querySelector('.restart');
+const minutesCls = document.querySelector('.minutes');
+const secondsCls = document.querySelector('.seconds');
+const movesCls = document.querySelector('.moves');
+const starsCls = document.querySelector('.stars');
 
 /*
  * Display the cards on the page
@@ -80,32 +84,32 @@ function reloadCards() {
     cardShown = [];
     clearInterval(timer);
     timer = setInterval(gameTimer, 1000);
-    document.querySelector('.minutes').textContent = '0';
-    document.querySelector('.seconds').textContent = '00';
-    document.querySelector('.moves').textContent = moves;
-    document.querySelector('.stars').innerHTML = calculateScore();
+    minutesCls.textContent = '0';
+    secondsCls.textContent = '00';
+    movesCls.textContent = moves;
+    starsCls.innerHTML = calculateScore();
 }
 
 function gameTimer() {
     if (cardShown.length !== cardsList.length && moves !== 0) {
         realSeconds += 1;
-    	if (realSeconds/60%1 === 0) {
-        	minutes += 1;
+        if (realSeconds / 60 % 1 === 0) {
+            minutes += 1;
             seconds = 0;
         } else {
-        	seconds += 1;
+            seconds += 1;
         }
         textSeconds = seconds.toString();
         if (seconds < 10) {
             textSeconds = `0${textSeconds}`
         }
-        document.querySelector('.minutes').textContent = minutes;
-        document.querySelector('.seconds').textContent = textSeconds;
+        minutesCls.textContent = minutes;
+        secondsCls.textContent = textSeconds;
     }
 }
 
 function displayCard() {
-    event.target.classList.add('show','open');
+    event.target.classList.add('show', 'open');
 }
 
 function addCardToList() {
@@ -114,7 +118,7 @@ function addCardToList() {
 
 function incrementMove() {
     moves += 1;
-    document.querySelector('.moves').textContent = moves;    
+    movesCls.textContent = moves;
 }
 
 function calculateScore() {
@@ -134,21 +138,21 @@ function calculateScore() {
 }
 
 function matchingCards() {
-    const firstCard = document.querySelector('#' + cardShown[cardShown.length-1]).parentElement;
-    const secondCard = document.querySelector('#' + cardShown[cardShown.length-2]).parentElement;
+    const firstCard = document.querySelector('#' + cardShown[cardShown.length - 1]).parentElement;
+    const secondCard = document.querySelector('#' + cardShown[cardShown.length - 2]).parentElement;
     firstCard.classList.add('match', 'animated', 'rubberBand');
     secondCard.classList.add('match', 'animated', 'rubberBand');
 }
 
 function notMatchingCards() {
-    const cardOne = document.querySelector('#' + cardShown[cardShown.length-1]).parentElement;
-    const cardTwo = document.querySelector('#' + cardShown[cardShown.length-2]).parentElement;
-    cardShown.splice(cardShown.length-2, 2);
+    const cardOne = document.querySelector('#' + cardShown[cardShown.length - 1]).parentElement;
+    const cardTwo = document.querySelector('#' + cardShown[cardShown.length - 2]).parentElement;
+    cardShown.splice(cardShown.length - 2, 2);
     cardOne.classList.add('unmatch', 'animated', 'wobble', 'blink');
     cardTwo.classList.add('unmatch', 'animated', 'wobble', 'blink');
-    window.setTimeout(function() {
-        cardOne.classList.remove('show','open','unmatch', 'animated', 'wobble', 'blink');
-        cardTwo.classList.remove('show','open','unmatch', 'animated', 'wobble', 'blink');
+    window.setTimeout(function () {
+        cardOne.classList.remove('show', 'open', 'unmatch', 'animated', 'wobble', 'blink');
+        cardTwo.classList.remove('show', 'open', 'unmatch', 'animated', 'wobble', 'blink');
     }, 1000);
 }
 
@@ -171,7 +175,7 @@ function gameOver() {
     winner.classList = 'game-over';
     document.body.appendChild(winner);
     document.querySelector('.game-over-text').classList.add('animated', 'bounceInDown');
-    document.querySelector('.play-again').addEventListener('click', function() {
+    document.querySelector('.play-again').addEventListener('click', function () {
         document.querySelector('.game-over').remove();
         reloadCards();
     });
@@ -180,20 +184,20 @@ function gameOver() {
 // Shuffle and load cards on page load
 reloadCards();
 
- // Add a click listener to the refresh button
+// Add a click listener to the refresh button
 restart.addEventListener('click', reloadCards);
 
 // Add a listener to the game board
-deck.addEventListener('click', function(event) {
+deck.addEventListener('click', function (event) {
     const targetClass = event.target.classList;
     const targetChildClass = event.target.firstElementChild.classList;
     if (targetClass[0] === "card" && targetClass[1] !== 'show') {
         displayCard();
         addCardToList();
-        if (cardShown.length%2 === 0) {
+        if (cardShown.length % 2 === 0) {
             const firstClick = document.querySelector('#' + cardShown[cardShown.length - 1]).classList[1];
             const secondClick = document.querySelector('#' + cardShown[cardShown.length - 2]).classList[1];
-            if(targetChildClass[1] === firstClick && targetChildClass[1] === secondClick) {
+            if (targetChildClass[1] === firstClick && targetChildClass[1] === secondClick) {
                 matchingCards();
             } else {
                 notMatchingCards();
